80fc01c6648a5d42bc6b8490e88bdf3fe193cd48
BRUNIER Terry <33851228+brunierterry@users.noreply.github.com>
2026-08-07 20:36:17 +0900

    Add visible application version 0.3.1

---
+++ app/assets/japanese-js-quest/version.js
@@ -0,0 +1,12 @@
+(function (root, factory) {
+  const version = factory()
+  if (typeof module === 'object' && module.exports) module.exports = version
+  else {
+    root.JSQuestVersion = version
+    const element = document.getElementById('app-version')
+    if (element) element.textContent = 'v' + version
+  }
+})(typeof self !== 'undefined' ? self : this, function () {
+  'use strict'
+  return '0.3.1'
+})
